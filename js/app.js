2f7194099c29a89a2a84debe62f4cba809627b0b
Alexandr Beletckiy <beletckiy111@gmail.com>
2022-01-06 13:19:20 +0200

    Scroll, slider

--- js/app.js
+++ js/app.js
@@ -3,15 +3,56 @@ $(function() {
 	let intro = $("#intro");
 	let introH = intro.innerHeight(); 
 	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+
+	checkScroll(scrollPos, introH);
 	
 	$(window).on("scroll load resize", function () {
 		let introH = intro.innerHeight(); 
 		scrollPos = $(this).scrollTop();
 	
+		checkScroll(scrollPos, introH);	
+	});
+
+	function checkScroll(scrollPos, introH) {
 		if (scrollPos > introH) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
 		}
+	}
+
+
+$("[data-scroll]").on("click", function (event) {
+	event.preventDefault();
+	let elementId = $(this).data("scroll");
+	let elementOffset = $(elementId).offset().top;
+	
+	nav.removeClass("show");
+
+	$("html, body").animate({
+		scrollTop: elementOffset - 70 
+	}, 700); 
+
 	});
+
+
+	navToggle.on("click", function(event) {
+		event.preventDefault();
+		nav.toggleClass("show");
+
+	});
+
+	
+		$("#clientSlider").slick({
+	  		infinite: true,
+	  		slidesToShow: 1,
+	  		slidesToScroll: 1,
+	  		fade: true,
+	  		arrows: false,
+	  		autoplay: true,
+  			autoplaySpeed: 2000,
+		});
+
 });
